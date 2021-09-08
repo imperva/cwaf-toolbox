a69e9091634310bb809c6051f895183770c59807
Brian Anderson <briananderson@Brians-Mac-mini.local>
2021-09-08 10:58:36 -0700

    updated variables file to have generic values.

--- nel-aws-for-new-relic/variables.tf
+++ nel-aws-for-new-relic/variables.tf
@@ -9,14 +9,14 @@ variable "account_id" {}
 variable "bucket_name" {}
 variable "aws_region" {}
 variable "algorithm" {default = "RSA"}
-variable "reg_email" {default = "joe.moore@imperva.com"}
-variable "subject_organization" {default = "Imperva"}
-variable "subject_organizational_unit" {default = "OCTO"}
+variable "reg_email" {default = "your@email.com"}
+variable "subject_organization" {default = "Your Company Name"}
+variable "subject_organizational_unit" {default = "Your_Business_Unit"}
 variable "subject_country" {default = "US"}
-variable "subject_postal_code" {default = "94403"}
+variable "subject_postal_code" {default = "12345"}
 variable "locality" {default = "San Mateo"}
 variable "serial_number" {default = "1"}
-variable "naked_domain" {default = "dev.impervademo.com"}
+variable "naked_domain" {default = "yourdomainhere.com"}
 variable "sub_domain" {default = "nel"}
 variable "report_to_params" {default = "account_id=$account_id&city=$city&country=$country&postalcode=$postalcode&state=$state&epoch=$epoch&longitude=$longitude&latitude=$latitude&site_id=$site_id&proxy_id=$proxy_id&pop=$pop&origin_pop=$origin_pop&session_id=$session_id&request_id=$request_id&asn=$asn"}
 variable "log_type" {default = "nel"}
